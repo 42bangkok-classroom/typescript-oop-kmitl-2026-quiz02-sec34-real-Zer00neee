fe2ff6e1ed8e7e82d5c0fdaef3174d4cb68f6d85
68010126 <68010126@kmitl.ac.th>
2026-01-16 19:58:17 +0700

    add

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,41 +1,109 @@
 import axios from "axios";
 
 type Geo = {
-  lat: string;
-  lng: string;
+  lat: string | null;
+  lng: string | null;
 };
 
 type Address = {
-  street: string;
-  suite: string;
-  city: string;
-  zipcode: string;
-  geo: Geo;
+  street: string | null;
+  suite: string | null;
+  city: string | null;
+  zipcode: string | null;
+  geo: Geo | null;
 };
 
-type UserApi = {
+type ApiUser = {
   id: number;
   name: string;
   phone: string;
-  address: Address;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
 };
 
-type User = {
+type NewUser = {
+  name?: string;
+  phone?: string;
+  address?: {
+    street?: string;
+    suite?: string;
+    city?: string;
+    zipcode?: string;
+    geo?: {
+      lat?: string;
+      lng?: string;
+    };
+  };
+} | null;
+
+type UserResult = {
   id: number;
-  name: string;
-  phone: string;
+  name: string | null;
+  phone: string | null;
   address: Address | null;
 };
 
-export async function getPostalAddress(): Promise<User[]> {
-  const res = await axios.get<UserApi[]>(
-    "https://jsonplaceholder.typicode.com/users"
-  );
 
-  return res.data.map((user) => ({
-    id: user.id,
-    name: user.name,
-    phone: user.phone,
-    address: user.address ?? null,
-  }));
+export async function addUser(newUserData: NewUser): Promise<UserResult[]> {
+  try {
+    const res = await axios.get<ApiUser[]>(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+
+    const users: UserResult[] = res.data.map((user) => ({
+      id: user.id,
+      name: user.name,
+      phone: user.phone,
+      address: {
+        street: user.address.street,
+        suite: user.address.suite,
+        city: user.address.city,
+        zipcode: user.address.zipcode,
+        geo: {
+          lat: user.address.geo.lat,
+          lng: user.address.geo.lng,
+        },
+      },
+    }));
+
+    if (newUserData === null) {
+      return users;
+    }
+
+    const lastId = users[users.length - 1]?.id ?? 0;
+
+    const newUser: UserResult = {
+      id: lastId + 1,
+      name: newUserData.name ?? null,
+      phone: newUserData.phone ?? null,
+      address: newUserData.address
+        ? {
+            street: newUserData.address.street ?? null,
+            suite: newUserData.address.suite ?? null,
+            city: newUserData.address.city ?? null,
+            zipcode: newUserData.address.zipcode ?? null,
+            geo: newUserData.address.geo
+              ? {
+                  lat: newUserData.address.geo.lat ?? null,
+                  lng: newUserData.address.geo.lng ?? null,
+                }
+              : null,
+          }
+        : null,
+    };
+
+    users.push(newUser);
+
+    return users;
+  } catch {
+    return [];
+  }
 }
