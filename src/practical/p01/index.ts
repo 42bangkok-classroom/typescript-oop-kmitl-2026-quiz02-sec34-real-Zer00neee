b94d74c81012134a4e01926c8e53f648f6773485
68010126 <68010126@kmitl.ac.th>
2026-01-16 19:53:29 +0700

    add

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,41 @@
-export function getPostalAddress() {}
+import axios from "axios";
+
+type Geo = {
+  lat: string;
+  lng: string;
+};
+
+type Address = {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+};
+
+type UserApi = {
+  id: number;
+  name: string;
+  phone: string;
+  address: Address;
+};
+
+type User = {
+  id: number;
+  name: string;
+  phone: string;
+  address: Address | null;
+};
+
+export async function getPostalAddress(): Promise<User[]> {
+  const res = await axios.get<UserApi[]>(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+
+  return res.data.map((user) => ({
+    id: user.id,
+    name: user.name,
+    phone: user.phone,
+    address: user.address ?? null,
+  }));
+}
