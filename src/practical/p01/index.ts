97a4d05185ad9e101d1eff193551300eba6cc594
68010126 <68010126@kmitl.ac.th>
2026-01-16 19:59:41 +0700

    add

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,109 +1,41 @@
 import axios from "axios";
 
 type Geo = {
-  lat: string | null;
-  lng: string | null;
+  lat: string;
+  lng: string;
 };
 
 type Address = {
-  street: string | null;
-  suite: string | null;
-  city: string | null;
-  zipcode: string | null;
-  geo: Geo | null;
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
 };
 
-type ApiUser = {
+type UserApi = {
   id: number;
   name: string;
   phone: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
+  address: Address;
 };
 
-type NewUser = {
-  name?: string;
-  phone?: string;
-  address?: {
-    street?: string;
-    suite?: string;
-    city?: string;
-    zipcode?: string;
-    geo?: {
-      lat?: string;
-      lng?: string;
-    };
-  };
-} | null;
-
-type UserResult = {
+type User = {
   id: number;
-  name: string | null;
-  phone: string | null;
+  name: string;
+  phone: string;
   address: Address | null;
 };
 
+export async function getPostalAddress(): Promise<User[]> {
+  const res = await axios.get<UserApi[]>(
+    "https://jsonplaceholder.typicode.com/users"
+  );
 
-export async function addUser(newUserData: NewUser): Promise<UserResult[]> {
-  try {
-    const res = await axios.get<ApiUser[]>(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-
-    const users: UserResult[] = res.data.map((user) => ({
-      id: user.id,
-      name: user.name,
-      phone: user.phone,
-      address: {
-        street: user.address.street,
-        suite: user.address.suite,
-        city: user.address.city,
-        zipcode: user.address.zipcode,
-        geo: {
-          lat: user.address.geo.lat,
-          lng: user.address.geo.lng,
-        },
-      },
-    }));
-
-    if (newUserData === null) {
-      return users;
-    }
-
-    const lastId = users[users.length - 1]?.id ?? 0;
-
-    const newUser: UserResult = {
-      id: lastId + 1,
-      name: newUserData.name ?? null,
-      phone: newUserData.phone ?? null,
-      address: newUserData.address
-        ? {
-            street: newUserData.address.street ?? null,
-            suite: newUserData.address.suite ?? null,
-            city: newUserData.address.city ?? null,
-            zipcode: newUserData.address.zipcode ?? null,
-            geo: newUserData.address.geo
-              ? {
-                  lat: newUserData.address.geo.lat ?? null,
-                  lng: newUserData.address.geo.lng ?? null,
-                }
-              : null,
-          }
-        : null,
-    };
-
-    users.push(newUser);
-
-    return users;
-  } catch {
-    return [];
-  }
+  return res.data.map((user) => ({
+    id: user.id,
+    name: user.name,
+    phone: user.phone,
+    address: user.address ?? null,
+  }));
 }
--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,8 +1,24 @@
-type newUser = {
+import axios from "axios";
+
+/* ---------- Types ---------- */
+type Geo = {
+  lat: string | null;
+  lng: string | null;
+};
+
+type Address = {
+  street: string | null;
+  suite: string | null;
+  city: string | null;
+  zipcode: string | null;
+  geo: Geo | null;
+};
+
+type ApiUser = {
+  id: number;
   name: string;
-  username?: string;
-  email?: string;
-  address?: {
+  phone: string;
+  address: {
     street: string;
     suite: string;
     city: string;
@@ -11,13 +27,86 @@ type newUser = {
       lat: string;
       lng: string;
     };
-  } | null;
-  phone: string;
-  website?: string;
-  company?: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
   };
 };
-export function addUser(newUser: newUser | null) {}
+
+type NewUser = {
+  name?: string;
+  phone?: string;
+  address?: {
+    street?: string;
+    suite?: string;
+    city?: string;
+    zipcode?: string;
+    geo?: {
+      lat?: string;
+      lng?: string;
+    };
+  };
+} | null;
+
+type UserResult = {
+  id: number;
+  name: string | null;
+  phone: string | null;
+  address: Address | null;
+};
+
+/* ---------- Function ---------- */
+export async function addUser(newUserData: NewUser): Promise<UserResult[]> {
+  try {
+    const res = await axios.get<ApiUser[]>(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+
+    // map users from API
+    const users: UserResult[] = res.data.map((user) => ({
+      id: user.id,
+      name: user.name,
+      phone: user.phone,
+      address: {
+        street: user.address.street,
+        suite: user.address.suite,
+        city: user.address.city,
+        zipcode: user.address.zipcode,
+        geo: {
+          lat: user.address.geo.lat,
+          lng: user.address.geo.lng,
+        },
+      },
+    }));
+
+    // edge case: newUserData === null
+    if (newUserData === null) {
+      return users;
+    }
+
+    const lastId = users[users.length - 1]?.id ?? 0;
+
+    const newUser: UserResult = {
+      id: lastId + 1,
+      name: newUserData.name ?? null,
+      phone: newUserData.phone ?? null,
+      address: newUserData.address
+        ? {
+            street: newUserData.address.street ?? null,
+            suite: newUserData.address.suite ?? null,
+            city: newUserData.address.city ?? null,
+            zipcode: newUserData.address.zipcode ?? null,
+            geo: newUserData.address.geo
+              ? {
+                  lat: newUserData.address.geo.lat ?? null,
+                  lng: newUserData.address.geo.lng ?? null,
+                }
+              : null,
+          }
+        : null,
+    };
+
+    users.push(newUser);
+
+    return users;
+  } catch {
+    return [];
+  }
+}
